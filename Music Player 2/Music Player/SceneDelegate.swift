90f7726e45190bcc1f26c7dd50aa8850b14715f3
Jane Jung <115315800+jane-jung@users.noreply.github.com>
2023-12-10 22:41:43 -0800

    Update SceneDelegate.swift

--- Music Player 2/Music Player/SceneDelegate.swift
+++ Music Player 2/Music Player/SceneDelegate.swift
@@ -26,22 +26,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
+    // Function cleans up or saves critical data before the app closes. 
+    // Function is called when the app's scene is disconnected (user closed the app or the system terminated it). 
     func sceneDidDisconnect(_ scene: UIScene) {
        
     }
 
+    // Function carries out tasks such as refreshing user interface or resuming any paused activities while app is in foreground. 
+    // Function is called when the app's scene becomes active. 
     func sceneDidBecomeActive(_ scene: UIScene) {
        
     }
 
+    // Function prepares the app for background mode (pauses animation, stops ongoing tasks, etc.).
+    // Function is called when the app's scene is about to resign its active status.
     func sceneWillResignActive(_ scene: UIScene) {
 
     }
 
+    // Function prepares the app for foreground mode (updates data, prepares UI for user interaction, etc.).
+    // Function is called right before the app's scene enters the foreground (becoming visible) again after being in the background. 
     func sceneWillEnterForeground(_ scene: UIScene) {
       
     }
 
+    // Function carries out tasks such as saving user data, stopping unneccessary processes or tasks, etc.
+    // Function is called when the app's scene enters the background (no longer visible). 
     func sceneDidEnterBackground(_ scene: UIScene) {
    
     }
