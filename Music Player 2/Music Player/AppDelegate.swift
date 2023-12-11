f1f10d66bc39892d8eb1bfe792290f9f6697909a
Jane Jung <115315800+jane-jung@users.noreply.github.com>
2023-12-10 22:47:13 -0800

    Update AppDelegate.swift

--- Music Player 2/Music Player/AppDelegate.swift
+++ Music Player 2/Music Player/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 // Class contains Application Methods.
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+// Function performs any additional setup after the application has launched (called after application launch). 
 // Function is called after application has launched. 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
      
@@ -20,12 +20,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 // Function is called when a new scene session is being created.
+// returns the configuration for a new scene session.
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
       
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
-// Function is called when the user discards a scene session. 
+// Function performs any necessary clean up when the user discards a scene session (called when user discards scene session).
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 
     }
