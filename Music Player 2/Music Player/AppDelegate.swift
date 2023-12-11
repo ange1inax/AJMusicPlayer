0a2cb73e6806ea000c6342b35f06ab074dd89582
Jane Jung <115315800+jane-jung@users.noreply.github.com>
2023-12-10 22:22:27 -0800

    Update AppDelegate.swift

--- Music Player 2/Music Player/AppDelegate.swift
+++ Music Player 2/Music Player/AppDelegate.swift
@@ -11,18 +11,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+// Function is called after application has launched. 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
      
         return true
     }
 
-
+// Function is called when a new scene session is being created.
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
       
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
+// Function is called when the user discards a scene session. 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 
     }
